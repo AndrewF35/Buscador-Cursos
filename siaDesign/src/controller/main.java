decfbf90329fa65d2281ae49222612492f0ae978
JJossa <75698621+JJossa@users.noreply.github.com>
2023-05-24 19:03:25 -0500

    Add files via upload
    GUI con Java Swing

---
+++ siaDesign/src/controller/main.java
@@ -0,0 +1,10 @@
+package controller;
+
+import GUI.login;
+
+public class main {
+    public static void main(String args[]){
+        login newLogin = new login();
+        newLogin.setVisible(true);
+    }    
+}
